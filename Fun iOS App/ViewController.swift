7be9cb67e3414c8c376f449b1fd630fd69312553
Karina Macancela <karymacancela@gmail.com>
2019-04-18 23:21:05 -0400

    Added label and button

--- Fun iOS App/ViewController.swift
+++ Fun iOS App/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func clickingTheLabel(_ sender: Any) {
+        theLabel.text = "Clicked the button!"
+    }
+    
 }
 
